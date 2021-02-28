427a6e3ce88ec725d03749c8422ec7ff909c3c6a
unknown <Natalia256>
2021-02-28 11:42:52 +0200

    Updates

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './styles.css';
-import './js/templates.js';
-import './js/localstorage.js';
--- src/js/templates.js
+++
@@ -1,6 +0,0 @@
-import itemsTemplate from '../templates/cards.hbs';
-import menus from '../menu.json';
-const markup = itemsTemplate(menus);
-
-const galleryRef = document.querySelector('.js-menu');
-galleryRef.insertAdjacentHTML('afterbegin', markup);
